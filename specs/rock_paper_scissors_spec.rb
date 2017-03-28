29972b858ac1a63ce62bfa5f217cfb1ffce194b5
Michael Shennan <m.shennan22@gmail.com>
2017-03-28 09:16:44 +0100

    tests now working

--- specs/rock_paper_scissors_spec.rb
+++ specs/rock_paper_scissors_spec.rb
@@ -4,20 +4,19 @@ require_relative('../models/rock_paper_scissors')
 
 class TestGame < MiniTest::Test
 
-  def setup
-    @game = Game.new()
-  end
-
   def test__rock_vs_paper
-    assert_equal("Paper wins!", @game.play("rock", "paper"))
+    @game = Game.new("rock", "paper")
+    assert_equal("Paper wins!", @game.play())
   end
 
   def test__scissors_vs_scissors
-    assert_equal("Draw!", @game.play("scissors", "scissors"))
+    @game = Game.new("scissors", "scissors")
+    assert_equal("Draw!", @game.play())
   end
 
   def test__scissors_vs_rock
-    assert_equal("Rock wins!", @game.play("scissors", "rock"))
+    @game = Game.new("scissors", "rock")
+    assert_equal("Rock wins!", @game.play())
   end
 
 end
